e623ed596fb1db859be7429089cc91d2624865d9
ShathaAlSaadan <shatha.9393@hotmail.com>
2019-01-05 18:00:08 +0300

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
 
@@ -15,13 +16,14 @@ class User(Base):
     name = Column(String(200), nullable=False)
     picture = Column(String(250))
 
+
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
     title = Column(String(250), nullable=False)
     User_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship(User, cascade="all, delete-orphan")
 
     @property
     def serialize(self):
@@ -31,21 +33,22 @@ class Category(Base):
             'id': self.id,
         }
 
+
 class Item(Base):
     __tablename__ = 'item'
 
     title = Column(String(250), nullable=False)
     id = Column(Integer, primary_key=True)
-    User_id= Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    User_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User, cascade="all, delete-orphan")
     Category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category)
+    category = relationship(Category, cascade="all, delete-orphan")
 
     @property
     def serialize(self):
         return{
-        'title' : self.title,
-        'id' : self.id
+         'title': self.title,
+         'id': self.id
         }
 
 engine = create_engine('sqlite:///categoryitmes.db')
